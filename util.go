himanshuprajapati-1/s3gof3r

commit 4ecf3c16aa1ebde1a25b2d4240595010598fd5d6

checkClose: check for nil io.Closer
The checkClose functioned used to check errors on deferred closes now
checks for a nil io.Closer interface before calling close.

fixes #69

util.go
@@ -77,9 +77,12 @@ func (e *RespError) Error() string {
 	)
 }
 
-func checkClose(c io.Closer, err *error) {
-	cerr := c.Close()
-	if *err == nil {
-		*err = cerr
+func checkClose(c io.Closer, err error) {
+	if c != nil {
+		cerr := c.Close()
+		if err == nil {
+			err = cerr
+		}
 	}
+
 }

auth.go
@@ -40,7 +40,7 @@ func InstanceKeys() (keys Keys, err error) {
 	if err != nil {
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		err = newRespError(resp)
 		return
@@ -56,7 +56,7 @@ func InstanceKeys() (keys Keys, err error) {
 	if err != nil {
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		err = newRespError(resp)
 		return

getter.go
@@ -76,7 +76,7 @@ func newGetter(getURL url.URL, c *Config, b *Bucket) (io.ReadCloser, http.Header
 	if err != nil {
 		return nil, nil, err
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		return nil, nil, newRespError(resp)
 	}
@@ -182,7 +182,7 @@ func (g *getter) getChunk(c *chunk) error {
 	if err != nil {
 		return err
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 206 {
 		return newRespError(resp)
 	}
@@ -321,7 +321,7 @@ func (g *getter) checkMd5() (err error) {
 	if err != nil {
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		return fmt.Errorf("MD5 check failed: %s not found: %s", md5Url.String(), newRespError(resp))
 	}

gof3r/cp.go
@@ -57,7 +57,7 @@ func (cp *cpOpts) Execute(args []string) (err error) {
 	if err != nil {
 		return
 	}
-	defer checkClose(src, &err)
+	defer checkClose(src, err)
 
 	dst, err := func(dst string) (io.WriteCloser, error) {
 		u, err := url.Parse(dst)
@@ -75,7 +75,7 @@ func (cp *cpOpts) Execute(args []string) (err error) {
 		return
 	}
 
-	defer checkClose(dst, &err)
+	defer checkClose(dst, err)
 	_, err = io.Copy(dst, src)
 	return
 }

gof3r/get.go
@@ -50,12 +50,12 @@ func (get *getOpts) Execute(args []string) (err error) {
 			return
 		}
 	}
-	defer checkClose(w, &err)
+	defer checkClose(w, err)
 	r, header, err := b.GetReader(get.Key, conf)
 	if err != nil {
 		return
 	}
-	defer checkClose(r, &err)
+	defer checkClose(r, err)
 	if _, err = io.Copy(w, r); err != nil {
 		return
 	}

gof3r/main.go
@@ -100,9 +100,11 @@ func getAWSKeys() (keys s3gof3r.Keys, err error) {
 	return
 }
 
-func checkClose(c io.Closer, err *error) {
-	cerr := c.Close()
-	if *err == nil {
-		*err = cerr
+func checkClose(c io.Closer, err error) {
+	if c != nil {
+		cerr := c.Close()
+		if err == nil {
+			err = cerr
+		}
 	}
 }

gof3r/put.go
@@ -49,12 +49,12 @@ func (put *putOpts) Execute(args []string) (err error) {
 			return
 		}
 	}
-	defer checkClose(r, &err)
+	defer checkClose(r, err)
 	w, err := b.PutWriter(put.Key, ACL(put.Header, put.ACL), conf)
 	if err != nil {
 		return
 	}
-	defer checkClose(w, &err)
+	defer checkClose(w, err)
 	if _, err = io.Copy(w, r); err != nil {
 		return
 	}

putter.go
@@ -85,7 +85,7 @@ func newPutter(url url.URL, h http.Header, c *Config, b *Bucket) (p *putter, err
 	if err != nil {
 		return nil, err
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		return nil, newRespError(resp)
 	}
@@ -200,7 +200,7 @@ func (p *putter) putPart(part *part) error {
 	if err != nil {
 		return err
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		return newRespError(resp)
 	}
@@ -252,7 +252,7 @@ func (p *putter) Close() (err error) {
 		p.abort()
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		p.abort()
 		return newRespError(resp)
@@ -298,7 +298,7 @@ func (p *putter) abort() {
 		logger.Printf("Error aborting multipart upload: %v\n", err)
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 204 {
 		logger.Printf("Error aborting multipart upload: %v", newRespError(resp))
 	}
@@ -343,7 +343,7 @@ func (p *putter) putMd5() (err error) {
 	if err != nil {
 		return
 	}
-	defer checkClose(resp.Body, &err)
+	defer checkClose(resp.Body, err)
 	if resp.StatusCode != 200 {
 		return newRespError(resp)
 	}

commit 7f7d20f990693adf49ee24f06b035a5be5f4c017

checkClose(): remove function (it is now unused)
This function was broken by

    4ecf3c1 (checkClose: check for nil io.Closer, 2015-07-20)

when it was changed to take its `err` parameter as a value rather than
as a pointer. This meant that its `err = cerr` only had a local
effect, and _didn't_ have the desired effect of causing its caller to
return an error. Therefore, many `Close()` errors would have been
lost.

The error was compounded by the many callers who used

    defer checkClose(resp.Body, err)

but where `err` wasn't a named return value. Therefore, even if the
caller had been changed to

    defer checkClose(resp.Body, &err)

, the deferred function invocation would not have had the desired
effect, because changing the local `err` variable would not have
changed the caller's return value anyway.

The commits leading up to this have fixed these errors at the callers
and removed users of `checkClose()`, changing the code to do explicit
error handling. Now we can remove the `checkClose()` function
entirely.

util.go
@@ -3,7 +3,6 @@ package s3gof3r
 import (
 	"encoding/xml"
 	"fmt"
-	"io"
 	"net/http"
 )
 
@@ -73,14 +72,4 @@ func (e *RespError) Error() string {
 		e.StatusCode,
 		e.Message,
 	)
-}
-
-func checkClose(c io.Closer, err error) {
-	if c != nil {
-		cerr := c.Close()
-		if err == nil {
-			err = cerr
-		}
-	}
-
 }